0c683a08d4e19ff5c5055b22da48e29268668ac8
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-22 01:47:16 -0600

    Initial commit

---
+++ lib/Alien/Graphene.pm
@@ -0,0 +1,18 @@
+package Alien::Graphene;
+# ABSTRACT: Alien package for the Graphene graphics math library
+
+use strict;
+use warnings;
+
+use base qw( Alien::Base );
+use Role::Tiny::With qw( with );
+
+with 'Alien::Role::Dino';
+
+1;
+__END__
+=head1 SEE ALSO
+
+L<Graphene|http://ebassi.github.com/graphene>
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Alien-Graphene/>
