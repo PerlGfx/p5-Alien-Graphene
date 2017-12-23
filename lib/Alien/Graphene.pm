6cbdd472377bf15c70d20a3cd271fdfa2828d14b
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-22 21:06:37 -0600

    Add gi_typelib_path class method

--- lib/Alien/Graphene.pm
+++ lib/Alien/Graphene.pm
@@ -9,6 +9,24 @@ use Role::Tiny::With qw( with );
 
 with 'Alien::Role::Dino';
 
+use File::Spec;
+
+=classmethod gi_typelib_path
+
+A path for using with L<Glib::Object::Introspection> to load the typelib
+needed.
+
+  use Env qw(@GI_TYPELIB_PATH);
+  push @GI_TYPELIB_PATH, Alien::Graphene->gi_typelib_path;
+
+=cut
+sub gi_typelib_path {
+	my ($class) = @_;
+	$class->install_type eq 'share'
+		? ( File::Spec->catfile( $class->dist_dir, qw(lib girepository-1.0) ) )
+		: ();
+}
+
 1;
 __END__
 =head1 SEE ALSO
